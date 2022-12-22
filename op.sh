97819e398ddfe4180b76a1e824e44cfffaba6af0
kimnhat2000 <35279642+kimnhat2000@users.noreply.github.com>
2022-12-22 12:59:03 -0500

    Update op.sh

--- op.sh
+++ op.sh
@@ -1,8 +1,4 @@
 #signin to 1Password.
 
 eval $(echo "$PASSWORD" | op account add --address $ADDRESS --email $EMAIL --secret-key $SECRET_KEY --signin)
-op vault list
-
-#create a test item
-
 op item create --category=securenote --title='new test item' --vault=v2qmba5ejlbqkznlepz7nllj4e "note=somedata"
